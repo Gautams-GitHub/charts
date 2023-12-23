7a64537b26c12d59d18ebece80d83bd984b40417
Gautams-GitHub <gagan.off.the.grid@gmail.com>
2023-12-23 14:27:05 +0530

    Optimized the code for DRY Principle, Adds Save Button and Pie Chart

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,6 +1,41 @@
-import Chart from 'chart.js/auto'
+import Chart from 'chart.js/auto';
 
-(async function() {
+function createChart(canvasId, chartType, data) {
+  const chartCanvas = document
+    .getElementById('acquisitions')
+    .appendChild(document.createElement('canvas'));
+  chartCanvas.id = canvasId;
+
+  new Chart(document.getElementById(canvasId), {
+    type: chartType,
+    data: {
+      labels: data.map((row) => row.year),
+      datasets: [
+        {
+          label: 'Acquisitions by year',
+          data: data.map((row) => row.count),
+        },
+      ],
+    },
+  });
+}
+
+function createSaveButton(canvasId, chartName) {
+  const saveButton = document
+    .getElementById('acquisitions')
+    .appendChild(document.createElement('button'));
+  saveButton.textContent = `Save ${chartName}`;
+  saveButton.addEventListener('click', () => {
+    const chartCanvas = document.getElementById(canvasId);
+    const dataURL = chartCanvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = `${chartName.toLowerCase()}_chart.png`;
+    link.click();
+  });
+}
+
+export async function acquisitions() {
   const data = [
     { year: 2010, count: 10 },
     { year: 2011, count: 20 },
@@ -11,19 +46,11 @@ import Chart from 'chart.js/auto'
     { year: 2016, count: 28 },
   ];
 
-  new Chart(
-    document.getElementById('acquisitions'),
-    {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-          }
-        ]
-      }
-    }
-  );
-})();
+  createChart('pie', 'pie', data);
+  createSaveButton('pie', 'Pie Chart');
+
+  createChart('bar', 'bar', data);
+  createSaveButton('bar', 'Bar Chart');
+}
+
+acquisitions();
